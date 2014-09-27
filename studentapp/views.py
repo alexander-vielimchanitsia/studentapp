dc5cd4136de4931aca22fdf2c24a544194c877d8
Alexander Vielimchanitsia <alexander_vielimchanitsia@mail.ua>
2014-09-27 11:47:47 +0300

    поправив forms

--- studentapp/views.py
+++ studentapp/views.py
@@ -10,6 +10,7 @@ def index(request):
     student_form = StudentForm
     args = {}
     args['studentapp_student'] = Student.objects.all()
+    args['form'] = student_form
     return render_to_response('index.html')
 
 def add_student(request):
