0091a8e7e27f0c6ec9fd283d6696c18c63a7cc4b
Alexander Vielimchanitsia <alexander_vielimchanitsia@mail.ua>
2014-11-21 00:04:23 +0200

    розбираюсь в crispy forms

--- studentapp/forms.py
+++ studentapp/forms.py
@@ -1,11 +1,27 @@
 from django import forms
 from django.forms import ModelForm
 from models import Student, Group
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field
+from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
 
 class StudentForm(ModelForm):
     class Meta:
         model = Student
         fields = ('first_name', 'last_name', 'middle_name', 'date', 'foto', 'stud_bilet', 'stud_group')
+    helper = FormHelper()
+    helper.form_class = 'form-horizontal'
+    helper.label_class = 'col-lg-2'
+    helper.field_class = 'col-lg-8'
+    helper.layout = Layout(
+        'first_name',
+        'last_name',
+        'middle_name',
+        'date',
+        'foto',
+        'stud_bilet',
+        'stud_group'
+    )
 
 class GroupForm(ModelForm):
     class Meta:
