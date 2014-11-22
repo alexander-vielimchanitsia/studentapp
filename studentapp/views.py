7fe20892bf189a88abd1f98540fa07ff37b762da
Alexander Vielimchanitsia <alexander_vielimchanitsia@mail.ua>
2014-11-22 17:49:03 +0200

    Працюю над джанго формами в редагуванні та з сортуванням, зробив округлення фото

--- studentapp/views.py
+++ studentapp/views.py
@@ -55,70 +55,78 @@ def addgroup(request):
                                                 })
 
 def edit_student(request, student_id):
-    table_student = Student.objects.get(id=student_id)
-    student_form = StudentForm()
-    errors = {}
-    error = {}
-    if request.POST.get('first_name', '').strip() == '':
-        errors['first_name'] = 'Введіть будь ласка ім’я студента'
-        error['first_name'] = 'поле Ім’я обов’язкоке'
-    if request.POST.get('last_name', '').strip() == '':
-        errors['last_name'] = 'Введіть будь ласка прізвище студента'
-        error['last_name'] = 'поле Прізвище обов’язкове'
-    if request.POST.get('middle_name', '').strip() == '':
-        errors['middle_name'] = 'Введіть будь ласка по батькові студента'
-        error['middle_name'] = 'поле По батькові обов’язкове'
-    if request.POST.get('date', '').strip() == '':
-        errors['date'] = 'Введіть будь ласка дату народження студента'
-        error['date'] = 'поле Дата народження обов’язкове'
-    if request.FILES.get('new_foto') > 0:
-        if not request.FILES.get('new_foto', None) or request.FILES['new_foto'].size == 0:
-            errors['foto'] = 'Виберіть будь ласка фото студента'
-            error['foto'] = 'поле Фото обов’язкове'
-    if request.POST.get('stud_bilet', '').strip() == '':
-        errors['stud_bilet'] = 'Введіть будь ласка ім’я студента'
-        error['stud_bilet'] = 'поле No.студ-билета обов’язкове'
-    if request.POST.get('stud_group', '').strip() == '':
-         errors['stud_group'] = 'Виберіть будь ласка групу студента'
-         error['stud_group'] = 'поле Група обов’язкове'
-    if request.POST.get('submit'):
-        if errors:return render_to_response('edit_student.html', {
-                                                            'errors': errors,
-                                                            'error': error,
-                                                            'table_student': table_student,
-                                                            'table_group': Group.objects.all(),
-                                                            'student_form': student_form,},
-                                                            context_instance=RequestContext(request)
-            )
-        else:
-            if request.FILES.get('new_foto') > 0:
-                student_object = Student(id=student_id,
-                                        first_name = request.POST['first_name'],
-                                        last_name = request.POST['last_name'],
-                                        middle_name = request.POST['middle_name'],
-                                        date = request.POST['date'],
-                                        foto = request.FILES['new_foto'],
-                                        stud_bilet = request.POST['stud_bilet'],
-                                        stud_group = Group(pk=request.POST['stud_group'])
-                )
-            else:
-                student_object = Student(id=student_id,
-                                        first_name = request.POST['first_name'],
-                                        last_name = request.POST['last_name'],
-                                        middle_name = request.POST['middle_name'],
-                                        date = request.POST['date'],
-                                        foto = table_student.foto,
-                                        stud_bilet = request.POST['stud_bilet'],
-                                        stud_group = Group(pk=request.POST['stud_group'])
-                )
-            student_object.save()
-            return redirect(u'/?status_message=Студент успішно відредагований!/')
-    return render_to_response('edit_student.html', {
-                                                        'table_student': table_student,
-                                                        'table_group': Group.objects.all(),
-                                                        'groups': groups},
-                                                        context_instance=RequestContext(request)
-        )
+    # table_student = Student.objects.get(id=student_id)
+    # student_form = StudentForm()
+    if request.POST:
+        form = Student.objects.get(id=student_id)
+        if form.is_valid():
+            form.save()
+            return redirect(u'/index/?status_message=Студент успішно відредагований!/')
+    else:
+        form = StudentForm()
+    return render(request, 'edit_student.html', {
+                                                'form': form,
+                                                })
+    # if request.POST.get('first_name', '').strip() == '':
+    #     errors['first_name'] = 'Введіть будь ласка ім’я студента'
+    #     error['first_name'] = 'поле Ім’я обов’язкоке'
+    # if request.POST.get('last_name', '').strip() == '':
+    #     errors['last_name'] = 'Введіть будь ласка прізвище студента'
+    #     error['last_name'] = 'поле Прізвище обов’язкове'
+    # if request.POST.get('middle_name', '').strip() == '':
+    #     errors['middle_name'] = 'Введіть будь ласка по батькові студента'
+    #     error['middle_name'] = 'поле По батькові обов’язкове'
+    # if request.POST.get('date', '').strip() == '':
+    #     errors['date'] = 'Введіть будь ласка дату народження студента'
+    #     error['date'] = 'поле Дата народження обов’язкове'
+    # if request.FILES.get('new_foto') > 0:
+    #     if not request.FILES.get('new_foto', None) or request.FILES['new_foto'].size == 0:
+    #         errors['foto'] = 'Виберіть будь ласка фото студента'
+    #         error['foto'] = 'поле Фото обов’язкове'
+    # if request.POST.get('stud_bilet', '').strip() == '':
+    #     errors['stud_bilet'] = 'Введіть будь ласка ім’я студента'
+    #     error['stud_bilet'] = 'поле No.студ-билета обов’язкове'
+    # if request.POST.get('stud_group', '').strip() == '':
+    #      errors['stud_group'] = 'Виберіть будь ласка групу студента'
+    #      error['stud_group'] = 'поле Група обов’язкове'
+    # if request.POST.get('submit'):
+    #     if errors:return render_to_response('edit_student.html', {
+    #                                                         'errors': errors,
+    #                                                         'error': error,
+    #                                                         'table_student': table_student,
+    #                                                         'table_group': Group.objects.all(),
+    #                                                         'student_form': student_form,},
+    #                                                         context_instance=RequestContext(request)
+    #         )
+    #     else:
+    #         if request.FILES.get('new_foto') > 0:
+    #             student_object = Student(id=student_id,
+    #                                     first_name = request.POST['first_name'],
+    #                                     last_name = request.POST['last_name'],
+    #                                     middle_name = request.POST['middle_name'],
+    #                                     date = request.POST['date'],
+    #                                     foto = request.FILES['new_foto'],
+    #                                     stud_bilet = request.POST['stud_bilet'],
+    #                                     stud_group = Group(pk=request.POST['stud_group'])
+    #             )
+    #         else:
+    #             student_object = Student(id=student_id,
+    #                                     first_name = request.POST['first_name'],
+    #                                     last_name = request.POST['last_name'],
+    #                                     middle_name = request.POST['middle_name'],
+    #                                     date = request.POST['date'],
+    #                                     foto = table_student.foto,
+    #                                     stud_bilet = request.POST['stud_bilet'],
+    #                                     stud_group = Group(pk=request.POST['stud_group'])
+    #             )
+    #         student_object.save()
+    #         return redirect(u'/?status_message=Студент успішно відредагований!/')
+    # return render_to_response('edit_student.html', {
+    #                                                     'table_student': table_student,
+    #                                                     'table_group': Group.objects.all(),
+    #                                                     'groups': groups},
+    #                                                     context_instance=RequestContext(request)
+    #     )
 
 
 def edit_group(request, group_id):
