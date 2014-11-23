874276eb6ea4d5c8ac70947a95da5be88e09b7f9
Alexander Vielimchanitsia <alexander_vielimchanitsia@mail.ua>
2014-11-23 23:41:22 +0200

    Переробив форму crispy forms, поправив кнопку та описав форму в файлі форм а не в шаблоні

--- studentapp/forms.py
+++ studentapp/forms.py
@@ -8,28 +8,26 @@ from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
 class StudentForm(ModelForm):
     class Meta:
         model = Student
-    helper = FormHelper()
-    helper.form_class = 'form-horizontal'
-    helper.label_class = 'col-lg-2'
-    helper.field_class = 'col-lg-2'
-    helper.button_class = 'col-lg-1'
-    helper.layout = Layout(
-        'first_name',
-        'last_name',
-        'middle_name',
-        'date',
-        'foto',
-        'stud_bilet',
-        'stud_group',
-    )
     def __init__(self, *args, **kwargs):
         super(StudentForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.form_id = 'id-exampleForm'
-        self.helper.form_class = 'blueForms'
+        self.helper.form_id = 'id-student-form'
+        self.helper.form_class = 'form-horizontal'
         self.helper.form_method = 'post'
         self.helper.form_action = '/index/addstudent/'
-        self.helper.add_input(Submit('submit', 'Submit'))
+        self.helper.add_input(Submit('submit', 'Submit', css_class="buttons-form"))
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-2'
+        self.helper.layout = Layout(
+            'first_name',
+            'last_name',
+            'middle_name',
+            'date',
+            'foto',
+            'stud_bilet',
+            'stud_group',
+        )
 
 class GroupForm(ModelForm):
     class Meta:
