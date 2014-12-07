ae0a3558f78d1bc587da2aaa2e7a7049449458e6
Alexander Vielimchanitsia <alexander_vielimchanitsia@mail.ua>
2014-12-07 23:57:20 +0200

    Більше менш зробив попап меню для додвання групи, залишилось доробити кнопку відправки. Поправив попап меню додавання студента

--- studentapp/static/js/pop-up-addgroup.js
+++ studentapp/static/js/pop-up-addgroup.js
@@ -1,16 +1,26 @@
 // POP-UP MENU ADD GROUP
-// $.ajax({
-//    type: "GET",
-//    url: "/groups/add/ #div_id_name_group",
-//    dataType: "html",
-//    success: function(data){
-//     // Open pop-up
-//     $("#button-add-group").click(function() {
-//         $("#div_id_name_group", data).fadeIn(300).appendTo("#modal-body").html();
-//         });
-//     }
-// });
-$(document).ready(function(){
-  $('#myModal').modal()
-});
+$.ajax({
+   type: "GET",
+   url: "/groups/add/ #div_id_name_group",
+   dataType: "html",
+   success: function(data){
+        // Open pop-up
+        $("#button-add-group").click(function() {
+            $("#div_id_name_group_popup").load("/groups/add/ #div_id_name_group");
+            $("#div_id_king_group_popup").load("/groups/add/ #div_id_king_group");
+            });
 
+        var str = find('input, select');
+
+        $("#button-add-group").click(function() {
+            $.ajax({
+                url: '/groups/add/',
+                type: 'POST',
+                data: str
+            })
+        });
+    }
+});
+// $(document).ready(function(){
+//   $('#myModal').modal()
+// });
--- studentapp/static/js/pop-up-addstudent.js
+++ studentapp/static/js/pop-up-addstudent.js
@@ -1,19 +1,18 @@
 // POP-UP MENU ADD STUDENT
 $.ajax({
    type: "GET",
-   url: "/addstudent/ #tab-content-addstudent",
+   url: "/students/add/ #tab-content-addstudent",
    dataType: "html",
    success: function(addstudent){
     // Open pop-up
     $("#button-add-student").click(function() {
         $("#tab-content-addstudent", addstudent).fadeIn(300).appendTo("#popup-box-addstudent").html();
         $("#ajax-fone-addstudent").fadeIn(300);
-        $("#popup-box-close-addstudent").fadeIn(300);
+        $("#popup-menu").fadeIn(300);
         });
     // Close pop-up
     $("#popup-box-close-addstudent, #ajax-fone-addstudent").click(function() {
-        $("#ajax-fone-addstudent, #popup-box-addstudent").fadeOut(300);
-        $("#popup-box-close-addstudent").fadeOut(300);
+        $("#ajax-fone-addstudent, #popup-menu").fadeOut(300);
     });
     }
 });
