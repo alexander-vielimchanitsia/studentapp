7713f640a0ede893223a5e4f03fccb52d674807f
Alexander Vielimchanitsia <alexander_vielimchanitsia@mail.ua>
2014-12-09 00:01:10 +0200

    Зробив збереження групи через попап меню, але поки не коректно працює, якщо валідація не проходить, та не показує меседж пілся додавання

--- studentapp/static/js/pop-up-addgroup.js
+++ studentapp/static/js/pop-up-addgroup.js
@@ -1,31 +1,23 @@
 // POP-UP MENU ADD GROUP
-$.ajax({
-   type: "GET",
-   url: "/groups/add/ #div_id_name_group",
-   dataType: "html",
-   success: function(){
-        // Open pop-up
-        $("#button-add-group").click(function() {
-            $("#div_id_name_group_popup").load("/groups/add/ #div_id_name_group");
-            $("#div_id_king_group_popup").load("/groups/add/ #div_id_king_group");
-            });
+// Open pop-up
+$("#button-add-group").click(function() {
+    $("#div_id_name_group_popup").load("/groups/add/ #div_id_name_group");
+    $("#div_id_king_group_popup").load("/groups/add/ #div_id_king_group");
+    });
 
-        $("#send-popup-form").click(function(e) {
-            e.preventDefault()
-            var mForm = $("#id-group-form-popup").serialize();
-            $.ajax({
-                type: 'POST',
-                data: mForm,
-                success: function(data){
-                    console.log(data)
-                },
-                error: function(data){
-                    console.log(data)
-                }
-            })
-        });
-    }
+$("#send-popup-form").click(function(e) {
+    e.preventDefault()
+    var mForm = $("#id-group-form-popup").serialize();
+    $.ajax({
+        url: '/groups/add/',
+        type: 'POST',
+        data: mForm,
+        success: function(data){
+            console.log(data)
+            $("#myModal").modal('hide');
+        },
+        error: function(data){
+            console.log('error')
+        }
+    })
 });
-// $(document).ready(function(){
-//   $('#myModal').modal()
-// });
