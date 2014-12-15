d7d50a9e7c47b883a1adb872eb796918dc2d5295
Alexander Vielimchanitsia <alexander_vielimchanitsia@mail.ua>
2014-12-15 22:15:09 +0200

    Пофіксив, щоб тултип ламався, коли поле назва групи завовнене

--- studentapp/static/js/pop-up-addgroup.js
+++ studentapp/static/js/pop-up-addgroup.js
@@ -7,21 +7,33 @@ $("#button-add-group").click(function() {
 
 $("#send-popup-form").click(function(e) {
     e.preventDefault()
-    var mForm = $("#id-group-form-popup").serialize();
+    var input = document.getElementsByName('name_group')[0],
+        mForm = $("#id-group-form-popup").serialize(),
+        textError = 'Поле обов’язкове';
     $.ajax({
         url: '/groups/add/',
         type: 'POST',
         data: mForm,
         success: function(data){
+            input.onfocus = function() {
+                    $("#id_name_group").tooltip({
+                        trigger: 'manual',
+                        placement: 'right',
+                        title: textError
+                    }).tooltip('show');
+            };
+            input.onblur = function() {
+                if ($('#id_name_group').val()!=="") {
+                    $("#id_name_group").tooltip('destroy');
+                }
+            };
             if ($('#id_name_group').val()!=="") {
-                $("#id_name_group").tooltip('destroy');
                 $("#myModal").modal('hide'); // Ховаємо попап меню.
                 $("#status-message-popup").show(); // Показуємо статус месседж.
                 $("#status-message-text").text("Група успішно додана!"); // Додаємо текст в статус месседж.
                 $("#content-columns").load("/groups/ #content-groups-list"); // Оновлюємо список груп.
             }
             else {
-                var textError = 'Введіть будь ласка назву групи';
                 $("#id_name_group").tooltip({
                     trigger: 'manual',
                     placement: 'right',
