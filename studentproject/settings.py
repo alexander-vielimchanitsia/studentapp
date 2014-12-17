66e3ea8786d6159adaa6568ab02114213316242f
Alexander Vielimchanitsia <alexander_vielimchanitsia@mail.ua>
2014-12-17 08:47:09 +0200

    Змінив базу даних sqlite3 на MySQL

--- studentproject/settings.py
+++ studentproject/settings.py
@@ -69,9 +69,16 @@ WSGI_APPLICATION = 'studentproject.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'students_db',
+        'USER': 'students_db_user',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
     }
 }
 
