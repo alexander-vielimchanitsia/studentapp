cd3dd640872ed8fc4c16e66f4df18898e23c49f1
Alexander Vielimchanitsia <alexander_vielimchanitsia@mail.ua>
2014-12-28 21:17:48 +0200

    Вставив іп Укрїни з бази даних в контекст процесор, і вивів його на усіх сторінках сайту

--- studentapp/utils/context_processors.py
+++ studentapp/utils/context_processors.py
@@ -1,14 +1,6 @@
 from django.contrib.gis.geoip import GeoIP
 
 
-# def get_client_ip(ip):
-#     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-#     if x_forwarded_for:
-#         ip = x_forwarded_for.split(',')[-1].strip()
-#     else:
-#         ip = request.META.get('REMOTE_ADDR')
-#     return ip
-
 def geoip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
@@ -18,5 +10,5 @@ def geoip(request):
 
     g = GeoIP()
     client_ip = ip
-    client_city = g.city(ip)
-    return {'client_ip': client_ip}
+    client_city = g.country_name('46.118.0.0')
+    return {'client_city': client_city}
