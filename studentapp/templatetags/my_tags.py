acfa2f575269adbdbf85572abb326b49548b2fe4
Alexander Vielimchanitsia <alexander_vielimchanitsia@mail.ua>
2015-01-11 00:03:27 +0200

    Зробив тег, який в шаблоні віднімає одиничку від змінної

--- studentapp/templatetags/my_tags.py
+++ studentapp/templatetags/my_tags.py
@@ -197,26 +197,26 @@ class WhileNode(template.Node):
 
 
 # SetVar
-class SetVarNode(template.Node):
-    def __init__(self, new_val, var_name):
-        self.new_val = new_val
-        self.var_name = var_name
+class MinusOneNode(template.Node):
+    def __init__(self, format_string):
+        self.format_string = format_string
+
     def render(self, context):
-        context[self.var_name] = self.new_val
-        return ''
+        try:
+            return int(self.format_string) - int(1)
+        except (TypeError, ValueError):
+            try:
+                return self.format_string - 1
+            except Exception:
+                return ''
 
 @register.tag
-def setvar(parser,token):
-    # This version uses a regular expression to parse tag contents.
+def minusone(parser,token):
     try:
-        # Splitting by None == splitting by spaces.
-        tag_name, arg = token.contents.split(None, 1)
+        # split_contents() knows not to split quoted strings.
+        tag_name, format_string = token.split_contents()
     except ValueError:
-        raise template.TemplateSyntaxError, "%r tag requires arguments" % token.contents.split()[0]
-    m = re.search(r'(.*?) as (\w+)', arg)
-    if not m:
-        raise template.TemplateSyntaxError, "%r tag had invalid arguments" % tag_name
-    new_val, var_name = m.groups()
-    if not (new_val[0] == new_val[-1] and new_val[0] in ('"', "'")):
-        raise template.TemplateSyntaxError, "%r tag's argument should be in quotes" % tag_name
-    return SetVarNode(new_val[1:-1], var_name)
+        raise template.TemplateSyntaxError("%r tag requires a single argument" % token.contents.split()[0])
+    if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
+        raise template.TemplateSyntaxError("%r tag's argument should be in quotes" % tag_name)
+    return MinusOneNode(format_string[1:-1])
