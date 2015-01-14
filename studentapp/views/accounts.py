7558b2d9959f2764dfac84ca5230d24c917ebf7a
Alexander Vielimchanitsia <alexander_vielimchanitsia@mail.ua>
2015-01-14 08:15:23 +0200

    Поперекривав список груп, додавання їх та додавання студента, користувачам які не залоговані. Змінив напрямки у вюшці на не захаркоджені

--- studentapp/views/accounts.py
+++ studentapp/views/accounts.py
@@ -2,13 +2,14 @@
 
 from django.shortcuts import render, HttpResponseRedirect, render_to_response, redirect
 from django.contrib.auth import logout, login, authenticate
+from django.core.urlresolvers import reverse
 
 from studentapp.forms.accounts import LoginForm, RegistrationForm
 
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect('/')
+    return HttpResponseRedirect(reverse('home'))
 
 def login_view(request):
     form = LoginForm(request.POST or None)
@@ -18,21 +19,23 @@ def login_view(request):
         password = form.cleaned_data['password']
         user = authenticate(username=username, password=password)
         login(request, user)
-        return redirect(u'/?status_message=Авторизація пройшла успішно!')
+        return redirect(u'%s?status_message=Авторизація пройшла успішно!' %
+                        reverse('home'))
 
     context = {
         'form': form
     }
-    return render(request, '../templates/accounts/login.html', context)
+    return render(request, 'accounts/login.html', context)
 
 def registration_view(request):
     form = RegistrationForm(request.POST or None)
 
     if form.is_valid():
         form.save()
-        return redirect(u'/?status_message=Користувач успішно зареєстрований!')
+        return redirect(u'%s?status_message=Користувач успішно зареєстрований!' %
+                        reverse('home'))
 
     context = {
         'form': form
     }
-    return render(request, '../templates/accounts/register.html', context)
+    return render(request, 'accounts/register.html', context)
