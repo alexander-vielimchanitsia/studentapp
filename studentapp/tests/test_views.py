90524d71d06407b16d3f135b52ef3a7a0360d3bd
Alexander Vielimchanitsia <alexander_vielimchanitsia@mail.ua>
2015-02-16 21:52:12 +0200

    Добавив 2 тести на вюшку

--- studentapp/tests/test_views.py
+++ studentapp/tests/test_views.py
@@ -1,13 +1,29 @@
+from django.core.files import File
+
 from django.test import TestCase, Client
 
-from studentapp.views.students import students_list
+from studentapp.models import Student, Group
 
 
 class HomePageTest(TestCase):
-    def test_homepage_is_available(self):
-        c = Client()
-        response = c.get('/')
-        self.assertEquals(response.status_code, 200)
+    def create_student(self):
+        f = open('studentapp/static/img/foto_icon.png') # photo of student
+
+        return Student.objects.create(
+            first_name='Luc',
+            last_name='Besson',
+            middle_name='Abramovich',
+            date='1965-05-27',
+            foto=File(f), # save photo in media
+            stud_bilet='1234',
+            stud_group=Group.objects.create(name_group='Name group'))
 
     def test_view_student_func(self):
-        pass
+        c = Client()
+        student = self.create_student()
+        response = c.get('/')
+
+        self.assertEquals(response.status_code, 200)
+
+        self.assertIn(student.first_name, response.content)
+        self.assertIn('</html>', response.content)
