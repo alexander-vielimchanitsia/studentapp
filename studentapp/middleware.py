2919e442b64f04d553207b8140316c942655bc8c
Alexander Vielimchanitsia <alexander_vielimchanitsia@mail.ua>
2015-02-16 22:38:33 +0200

    Поправив мідлвару, тепер коли якась помилка, то буде перехвачувати її і вертати None

--- studentapp/middleware.py
+++ studentapp/middleware.py
@@ -7,9 +7,7 @@ import re
 
 
 class StatsMiddleware(object):
-
     def process_view(self, request, view_func, view_args, view_kwargs):
-
         n = len(connection.queries) # number queries database
 
         start = time()
@@ -17,26 +15,28 @@ class StatsMiddleware(object):
         response = view_func(request, *view_args, **view_kwargs)
 
         if response['content-type'] == 'text/html; charset=utf-8':
+            try:
+                tot_time = time() - start
 
-            tot_time = time() - start
+                db_queries = len(connection.queries) - n
 
-            db_queries = len(connection.queries) - n
+                if db_queries:
+                    db_time = reduce(add, [float(q['time'])
+                                           for q in connection.queries[n:]])
+                else:
+                    db_time = 0.0
 
-            if db_queries:
-                db_time = reduce(add, [float(q['time'])
-                                       for q in connection.queries[n:]])
-            else:
-                db_time = 0.0
+                # and backout python time
+                python_time = tot_time - db_time
 
-            # and backout python time
-            python_time = tot_time - db_time
-
-            response.content = response.content.replace('<body>',
-                '<body><div id="rendering_page_text">Весь час генерації сторінки: %.2f, '
-                'Python: %.2f, DB: %.2f, Всього запитів: %.d</div>'
-                % (tot_time, python_time, db_time, db_queries))
+                response.content = response.content.replace('<body>',
+                    '<body><div id="rendering_page_text">Весь час генерації сторінки: %.2f, '
+                    'Python: %.2f, DB: %.2f, Всього запитів: %.d</div>'
+                    % (tot_time, python_time, db_time, db_queries))
+            except Exception:
+                return None
 
             return response
 
         else:
-            return response
+            return None
