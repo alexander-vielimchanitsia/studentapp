d012099f19a6965ba93c32c00eab21266f6fd1a7
Alexander Vielimchanitsia <alexander_vielimchanitsia@mail.ua>
2015-03-07 17:53:02 +0200

    migrated

---
+++ studentapp/migrations/0005_auto_20150307_1551.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('studentapp', '0004_auto_20150228_2104'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='group',
+            options={'verbose_name': '\u0413\u0440\u0443\u043f\u0430', 'verbose_name_plural': '\u0413\u0440\u0443\u043f\u0438'},
+        ),
+    ]
